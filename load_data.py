e4b4ffe4115c83c3a080315bb08ec2081cdb7028
deeroen <cocoloulouj@hotmail.com>
2019-11-28 14:15:03 +0100

    28/11

---
+++ load_data.py
@@ -0,0 +1,8 @@
+import sys
+working_dirr = "C:\\Users\cocol\Desktop\Q1_ingé_civil_M2\Machine learning\project"
+sys.path.append(working_dirr)
+import pandas as pd
+X1 = pd.read_csv(working_dirr+"\Datasets-20191122\X1.csv")
+Y1 = pd.read_csv(working_dirr+"\Datasets-20191122\Y1.csv")
+X2 = pd.read_csv(working_dirr+"\Datasets-20191122\X2.csv")
+
