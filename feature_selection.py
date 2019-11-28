d2a3eb6f4d6dad7b5e4602a07e475318030cb197
deeroen <cocoloulouj@hotmail.com>
2019-11-28 15:30:34 +0100

    28/11 2

---
+++ feature_selection.py
@@ -0,0 +1,33 @@
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+from sklearn import feature_selection, tree
+import pandas as pd
+from sklearn import preprocessing
+
+import numpy as np
+
+
+def features_selection(features_df, target, nb_of_features):
+    # normalize the table
+    min_max_scaler = preprocessing.MinMaxScaler()
+    scaled_df = pd.DataFrame(min_max_scaler.fit_transform(features_df))
+    scaled_df.columns = features_df.columns
+
+    # Select with mutual information
+    selector = SelectKBest(feature_selection.mutual_info_regression, k=nb_of_features)
+    selector.fit_transform(scaled_df, target)
+    # Get columns to keep
+    cols1 = scaled_df.columns[selector.get_support(indices=True)]
+
+    # Select with F-value between label/feature for regression tasks
+    selector = SelectKBest(feature_selection.f_regression, k=nb_of_features)
+    selector.fit_transform(scaled_df, target)
+    # Get columns to keep
+    cols2 = scaled_df.columns[selector.get_support(indices=True)]
+
+
+    features = list(set(list(cols1)) | set(list(cols2)))
+    # Create new dataframe with only desired columns, or overwrite existing
+    a_scaled = features_df[features]
+
+    return a_scaled
--- load_data.py
+++ load_data.py
@@ -4,5 +4,10 @@ sys.path.append(working_dirr)
 import pandas as pd
 X1 = pd.read_csv(working_dirr+"\Datasets-20191122\X1.csv")
 Y1 = pd.read_csv(working_dirr+"\Datasets-20191122\Y1.csv")
-X2 = pd.read_csv(working_dirr+"\Datasets-20191122\X2.csv"
+X2 = pd.read_csv(working_dirr+"\Datasets-20191122\X2.csv")
+Y1bis = Y1.append({"3.0":123}, ignore_index=True)
+from feature_selection import features_selection
 
+worktbl = pd.concat([X1.drop(['wd'], axis=1), pd.get_dummies(X1['wd'])], axis=1)
+
+tbl = features_selection(worktbl, Y1bis, 7)
