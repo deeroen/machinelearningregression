b2c5ed7ca82199f38c705793ef6714bfef5ec610
Noemie <noemie.verstraete@student.uclouvain.be>
2019-11-28 16:55:40 +0100

    ajout des fichiers utiles solutions des TPs: - competitive learning - linear model - rbfn - show_quantization

--- load_data.py
+++ load_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
-X1 = pd.read_csv("Datasets-20191122\X1.csv")
-Y1 = pd.read_csv("Datasets-20191122\Y1.csv",sep='\t',names=["Label"])
-X2 = pd.read_csv("Datasets-20191122\X2.csv")
+X1 = pd.read_csv("Datasets-20191122/X1.csv")
+Y1 = pd.read_csv("Datasets-20191122/Y1.csv",sep='\t',names=["Label"])
+X2 = pd.read_csv("Datasets-20191122/X2.csv")
 
 from feature_selection import features_selection
 
