7de778e07cc9e54163aeaa0ee12f5e637d24279d
deeroen <cocoloulouj@hotmail.com>
2019-11-28 14:23:34 +0100

    28/11

--- load_data.py
+++ load_data.py
@@ -4,5 +4,5 @@ sys.path.append(working_dirr)
 import pandas as pd
 X1 = pd.read_csv(working_dirr+"\Datasets-20191122\X1.csv")
 Y1 = pd.read_csv(working_dirr+"\Datasets-20191122\Y1.csv")
-X2 = pd.read_csv(working_dirr+"\Datasets-20191122\X2.csv")
+X2 = pd.read_csv(working_dirr+"\Datasets-20191122\X2.csv"
 
