a89006956f6a6b7b7420e6d0299bc0c3907a343b
deeroen <cocoloulouj@hotmail.com>
2019-11-28 15:39:00 +0100

    28/11 dataset

--- load_data.py
+++ load_data.py
@@ -1,13 +1,10 @@
-import sys
-working_dirr = "C:\\Users\cocol\Desktop\Q1_ingé_civil_M2\Machine learning\project"
-sys.path.append(working_dirr)
 import pandas as pd
-X1 = pd.read_csv(working_dirr+"\Datasets-20191122\X1.csv")
-Y1 = pd.read_csv(working_dirr+"\Datasets-20191122\Y1.csv")
-X2 = pd.read_csv(working_dirr+"\Datasets-20191122\X2.csv")
-Y1bis = Y1.append({"3.0":123}, ignore_index=True)
+X1 = pd.read_csv("Datasets-20191122\X1.csv")
+Y1 = pd.read_csv("Datasets-20191122\Y1.csv",sep='\t',names=["Label"])
+X2 = pd.read_csv("Datasets-20191122\X2.csv")
+
 from feature_selection import features_selection
 
 worktbl = pd.concat([X1.drop(['wd'], axis=1), pd.get_dummies(X1['wd'])], axis=1)
 
-tbl = features_selection(worktbl, Y1bis, 7)
+tbl = features_selection(worktbl, Y1, 7)
